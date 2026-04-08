87a903fd94cf3f059c5e70c2b71a08f9362d3b4f
Manoj Tarkar <your-email@example.com>
2026-04-08 13:31:07 +0530

    Full project upload

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const port =4000
 
 // middleware
 app.use(express.json())
-// app.use(cors())
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true
 }));
 
