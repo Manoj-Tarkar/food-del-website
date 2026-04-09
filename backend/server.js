2b1539e02b6101bc62da13bca5fbc5f24db0f929
Manoj-Tarkar <tarkarmanoj2006@gmail.com>
2026-04-10 00:32:53 +0530

    added new features and updates

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
-
+import orderRouter from "./routes/orderRoute.js"
 
 
 // app config
@@ -14,9 +14,9 @@ const port =4000
 
 // middleware
 app.use(express.json())
-// app.use(cors())
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true
 }));
 
@@ -28,7 +28,7 @@ app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
-
+app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
